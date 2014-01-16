17a8b1b8168e562aa11a9e4d8d96db33620f11d1
Amit Apple <amitap@microsoft.com>
2014-01-15 19:30:12 -0800

    Demo of a simple .NET consople application which is git deployable as a web job

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Working on " + ConfigurationManager.AppSettings["Work"]);
+                Console.WriteLine("Connection string is " + ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
